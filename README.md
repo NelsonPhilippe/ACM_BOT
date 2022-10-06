b688e18cfd82471701782ea48b09567a0b54c56b
XiliTra <philippenelson59@gmail.com>
2022-10-06 19:44:18 +0200

    initial commit, add discord dependancies

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ACM_BOT
 Bot discord for my discord, this discord is for my friends :)
+
+During this project, I will upgrade my level in Javascript & English :)
---
+++ index.js
@@ -0,0 +1,8 @@
+const { REST, Routes } = require('discord.js')
+
+const commands = [
+    {
+        name : "ping",
+        description: "Permet de vérifier le bon fonctionnement du bot discord"
+    }
+]
