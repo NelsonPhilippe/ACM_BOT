c86be58a2cafeddbb9c8d9dd303ffee5523ec7e0
Nelson Philippe <nenelphinel59@gmail.com>
2022-10-07 17:56:41 +0200

    add file commannd for prepare working space, Object Command is a command manager/builder for create discord command

---
+++ command/Commands.js
@@ -0,0 +1,13 @@
+
+
+exports.Command = class Command {
+
+    constructor(name, description, options) {
+        this.name = name;
+        this.description = description;
+        this.options = options;
+    }
+
+
+
+}
