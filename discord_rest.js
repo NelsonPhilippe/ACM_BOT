50643f53270fc34ca72487ab3780700358d2ab89
XiliTra <philippenelson59@gmail.com>
2022-10-16 16:35:17 +0200

    docs(common) : add docs for command object

--- discord_rest.js
+++ discord_rest.js
@@ -14,14 +14,16 @@ exports.commands = [
 const rest = new REST({ version : 10 }).setToken(discord_token);
 
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(Routes.applicationCommands(discord_client_id), { body: commands });
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+exports.register_commands = () => {
+    (async () => {
+        try {
+            console.log('Started refreshing application (/) commands.');
+    
+            await rest.put(Routes.applicationCommands(discord_client_id), { body: commands });
+    
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+}
--- index.js
+++ index.js
@@ -5,16 +5,18 @@ require('dotenv').config()
 exports.discord_token = process.env.DISCORD_TOKEN
 exports.discord_client_id = process.env.DISCORD_CLIENT_ID
 
+const {register_commands} = require('./discord_rest')
 
 
-require('./discord_rest')
+// register commands instantiate by rest api discord
+register_commands()
 
-
-
+// start client discord
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
+// command event
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
